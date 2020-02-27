1882e50306215c265e21d070e539c791da0c0dee
jyangit <46543626+jyangit@users.noreply.github.com>
2020-02-26 18:45:43 -0800

    Initial commit

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        questionLabel.text = quizBrain.quizQuestions[0].text
+        
+        updateUI()
+        
+    }
+    
+    var timer = Timer()
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        let userGotItRight = quizBrain.checkAnswer(answer: userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+           
+        }else{
+            sender.backgroundColor = UIColor.red
+            
+        }
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    
+        var doWeRestart = quizBrain.nextQuestion()
+        
+        if doWeRestart == 0
+        {
+            updateUI()
+        }
+    }
+    
+    var timePassed: Float = 0.0
+    
+    @objc func updateUI(){
+        
+        questionLabel.text = quizBrain.getQuestionText()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        
+    }
+
+}
+
---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,67 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by Marcus Kim on 2/5/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct QuizBrain {
+
+    let quizQuestions = [
+           Question(q: "A slug's blood is green.", a: "True"),
+           Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+           Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+           Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+           Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+           Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+           Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+           Question(q: "Google was originally called 'Backrub'.", a: "True"),
+           Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+           Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+           Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+           Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+           Question(q: "We can learn programming on Youtube.com", a: "True")
+           Question(q: "The csuf is a good school", a: "True")
+
+       ]
+
+    var questionNumber = 0
+    var score = 0
+
+    mutating func checkAnswer(answer: String) -> Bool {
+        if answer == quizQuestions[questionNumber].answer{
+            score += 1
+            return true
+        }else{
+            return false
+        }
+    }
+
+    func getQuestionText() -> String {
+        return quizQuestions[questionNumber].text
+    }
+
+    func getProgress() -> Float {
+        let progress = Float(questionNumber + 1)/Float(quizQuestions.count)
+
+        return progress
+    }
+
+    mutating func nextQuestion() -> Int{
+        if questionNumber + 1 < quizQuestions.count {
+            questionNumber += 1
+            return questionNumber
+        } else {
+            questionNumber = 0
+            score = 0
+            return questionNumber
+        }
+    }
+
+    func getScore() -> Int{
+        return score
+    }
+
+}
