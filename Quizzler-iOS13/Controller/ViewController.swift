474f1f3439c4fb2c192bad403389801bbad739b8
jyangit <46543626+jyangit@users.noreply.github.com>
2020-02-29 14:03:33 -0800

    update the userGotItRight logic

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -9,59 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
-    
+
     var quizBrain = QuizBrain()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         questionLabel.text = quizBrain.quizQuestions[0].text
-        
+
         updateUI()
-        
+
     }
-    
+
     var timer = Timer()
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
+
         let userAnswer = sender.currentTitle!
         let userGotItRight = quizBrain.checkAnswer(answer: userAnswer)
-        
+
+        if userGotItRight == null{
+          sender.backgroundColor = UIColor.red;
+          userAnswer = "The user Answer is invalid";
+        }
+
         if userGotItRight {
-            sender.backgroundColor = UIColor.green
-           
+            sender.backgroundColor = UIColor.blue
+
         }else{
             sender.backgroundColor = UIColor.red
-            
+
         }
-        
+
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-    
+
         var doWeRestart = quizBrain.nextQuestion()
-        
+
         if doWeRestart == 0
         {
             updateUI()
         }
     }
-    
+
     var timePassed: Float = 0.0
-    
+
     @objc func updateUI(){
-        
+
         questionLabel.text = quizBrain.getQuestionText()
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.getScore())"
-        
+
     }
 
 }
-
