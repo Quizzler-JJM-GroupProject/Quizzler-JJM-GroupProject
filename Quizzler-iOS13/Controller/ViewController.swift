672b810d08551d225e1b9bc98dbbdc7299d659cd
jyangit <46543626+jyangit@users.noreply.github.com>
2020-03-06 22:24:12 -0800

    add a loop statement for number with 10

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -9,59 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
-    
+
     var quizBrain = QuizBrain()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         questionLabel.text = quizBrain.quizQuestions[0].text
-        
+
         updateUI()
-        
+
     }
-    
+
     var timer = Timer()
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
+
         let userAnswer = sender.currentTitle!
         let userGotItRight = quizBrain.checkAnswer(answer: userAnswer)
-        
+
         if userGotItRight {
             sender.backgroundColor = UIColor.green
-           
+
         }else{
             sender.backgroundColor = UIColor.red
-            
+
         }
-        
+
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
-    
+
         var doWeRestart = quizBrain.nextQuestion()
-        
+
+        int num=1;	//initializing the variable
+        while(num<=10)	//while loop with condition
+        {
+          printf("%d\n",num);
+          num++;		//incrementing operation
+        }
+        return 0;
+
         if doWeRestart == 0
         {
             updateUI()
         }
     }
-    
+
     var timePassed: Float = 0.0
-    
+
     @objc func updateUI(){
-        
+
         questionLabel.text = quizBrain.getQuestionText()
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
         progressBar.progress = quizBrain.getProgress()
         scoreLabel.text = "Score: \(quizBrain.getScore())"
-        
+
     }
 
 }
-
